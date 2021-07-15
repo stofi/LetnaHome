d64568bff7122f9763159ffed9123f9b89207449
Krystof Rainisch <Krystof.Rainisch@hey.com>
2021-07-15 14:56:34 +0200

    add state

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <Renderer ref="renderer" :resize="'window'" antialias shadow>
-    <Camera
-      :position="{ z: -0.2, y: 0.16, x: 0.55 }"
-      :lookAt="{ z: 0, y: 0.17, x: 0 }"
-    />
+  <Renderer
+    ref="renderer"
+    :resize="'window'"
+    antialias
+    shadow
+    :pointer="{ intersectRecursive: true }"
+  >
+    <Camera :position="cameraPos" :lookAt="lookAtPos" />
     <Scene ref="scene" background="#181818">
       <Plane
         :position="{ x: 0, z: 0, y: 0 }"
         :width="10"
         :height="10"
-        recieve-shadow
         :rotation="{ x: -Math.PI / 2, y: 0, z: 0 }"
         ><PhysicalMaterial color="#eee" /> </Plane
       >>
@@ -24,13 +26,10 @@
       <!-- <RectAreaLight :position="{ x: 50, z: 50 }" cast-shadow :intensity="1" /> -->
       <!-- <AmbientLight cast-shadow :intensity="0.1" /> -->
       <GltfModel
+        ref="model"
         src="/mac128.gltf"
-        @load="onModelLoaded"
-        @progress="onProgress"
-        @error="onError"
         :position="{ x: 0, z: 0 }"
-        cast-shadow
-        recieve-shadow
+        @click="onClick"
       />
     </Scene>
   </Renderer>
@@ -39,6 +38,7 @@
 <script>
 /* eslint-disable vue/no-unused-components */
 /* eslint-disable no-unused-vars */
+
 import {
   GltfModel,
   Box,
@@ -78,6 +78,49 @@ export default {
     PhysicalMaterial,
     InstancedMesh,
     Plane,
+  },
+  data() {
+    return {
+      timeout: null,
+      cameraSwitch: false,
+      cameraStartPos: { z: -0.2, y: 0.16, x: 0.55 },
+      cameraEndPos: { z: 0.0, y: 0.25, x: 0.15 },
+      lookAtStartPos: { z: 0, y: 0.17, x: 0 },
+      lookAtEndPos: { z: 0, y: 0.2258, x: 0 },
+    }
+  },
+  computed: {
+    cameraPos() {
+      return this.cameraSwitch ? this.cameraStartPos : this.cameraEndPos
+    },
+    lookAtPos() {
+      return this.cameraSwitch ? this.lookAtStartPos : this.lookAtEndPos
+    },
+    pointer() {
+      return this.$refs.renderer.three.pointer
+    },
+    test() {
+      return this.$refs.scene.scene.children
+        .find(({ name }) => name === 'Scene')
+        .children.find(({ name }) => name === 'Glass')
+    },
+  },
+  mounted() {
+    this.setupPointer()
+  },
+  methods: {
+    positionEqual(pos1, pos2) {
+      return pos1.x === pos2.x && pos1.y === pos2.y && pos1.z === pos2.z
+    },
+    setupPointer() {
+      // console.log(this.pointer)
+    },
+    onClick() {
+      clearTimeout(this.timeout)
+      this.timeout = setTimeout(() => {
+        this.cameraSwitch = !this.cameraSwitch
+      }, 0)
+    },
   },
 }
 </script>
